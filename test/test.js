463011b0c27b187128cd4153f28a46f42c0e32e5
Miguel Diaz <mdiaz.git+thor@codescrum.com>
2019-12-03 17:43:11 -0500

    creating BitmaskSpace and Bitmask classes and basic testing

--- test/test.js
+++ test/test.js
@@ -1,8 +1,44 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const { BitmaskSpace, Bitmask } = require('../lib/index')
+
+describe('BitmaskSpace', function() {
+  describe('#constructor', function() {
+    it('throws an error if constructed with `undefined`', function() {
+      assert.throws(function(){ new BitmaskSpace() }, Error, 'The space defined by all elements must be an array!');
+      assert.throws(function(){ new BitmaskSpace(undefined) }, Error, 'The space defined by all elements must be an array!');
+    });
+    it('throws an error if constructed with `null`', function() {
+      assert.throws(function(){ new BitmaskSpace(null) }, Error, 'The space defined by all elements must be an array!');
+    });
+    it('throws an error if constructed with an empty array `[]`', function() {
+      assert.throws(function(){ new BitmaskSpace([]) }, Error, 'The space defined by all elements cannot be empty!');
+    });
+    it('correctly populates all_elements, size, ones, zeros, options', function() {
+      let bitmask_space = new BitmaskSpace([1,2,3,4], {sort: true})
+      assert(bitmask_space.elements.every(function(element){ return [1,2,3,4].includes(element)}))
+      assert.equal(bitmask_space.options['sort'], true)
+      assert.equal(bitmask_space.size, 4)
+      assert.equal(bitmask_space.ones,  "1111")
+      assert.equal(bitmask_space.zeros, "0000")
+    });
+    it('correctly populates all_elements, size, ones, zeros, and default options', function() {
+      let bitmask_space = new BitmaskSpace([1,2,3,4])
+      assert(bitmask_space.elements.every(function(element){ return [1,2,3,4].includes(element)}))
+      assert.equal(bitmask_space.options['sort'], false)
+      assert.equal(bitmask_space.size, 4)
+      assert.equal(bitmask_space.ones,  "1111")
+      assert.equal(bitmask_space.zeros, "0000")
     });
   });
+
+  describe('#bitmask', function() {
+    it('creates a new bitmask, whose space is `this`', function() {
+      let bitmask_space = new BitmaskSpace([1,2,3,4])
+      let bitmask = bitmask_space.bitmask([1,2])
+      assert(bitmask instanceof Bitmask)
+      assert(bitmask.space instanceof BitmaskSpace)
+      assert.equal(bitmask.space, bitmask_space)
+    });
+  });
+
 });
